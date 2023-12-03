ae7de98b5c34a192e679b2febbe0ce8d1c4cb77c
knalli <knallisworld@googlemail.com>
2023-12-03 17:20:50 +0100

    Add common pre computed output helper

--- src/main/java/de/knallisworld/aoc2023/support/cli/Commons.java
+++ src/main/java/de/knallisworld/aoc2023/support/cli/Commons.java
@@ -19,7 +19,16 @@ public class Commons {
 		final var started = System.currentTimeMillis();
 		final var result = solutionGet.get();
 		final var used = System.currentTimeMillis() - started;
-		System.out.printf("🎉 Solution of part %s took %7.3fs: %s%n", part, used/1000D, result);
+		System.out.printf("🎉 Solution of part %s took %7.3fs: %s%n", part, used / 1000D, result);
+	}
+
+	public static <T> T compileObject(final Serializable description,
+									  final Supplier<T> compiler) {
+		final var started = System.currentTimeMillis();
+		final var result = compiler.get();
+		final var used = System.currentTimeMillis() - started;
+		System.out.printf("💪 Compiling object '%s' took %7.3fs%n", description, used / 1000D);
+		return result;
 	}
 
 }
