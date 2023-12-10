edb16652b34e87eccaf5c3d6330024487f226233
knalli <knallisworld@googlemail.com>
2023-12-10 15:12:54 +0100

    Improve memory footprint of Point2D
    Memory usage of millions of (same) instances are reduced drastically
    * ensure using an internal cache (immutables instances as record, so its fine)
    * avoid creating lambada instances for each instance

--- src/main/java/de/knallisworld/aoc2023/support/geo/Point2D.java
+++ src/main/java/de/knallisworld/aoc2023/support/geo/Point2D.java
@@ -2,61 +2,94 @@ package de.knallisworld.aoc2023.support.geo;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.springframework.lang.Nullable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiFunction;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNullElse;
 
 @Data
 @EqualsAndHashCode(of = {"x", "y"})
 public class Point2D<T extends Number> {
 
+	static final Map<Number, Map<Number, Point2D<?>>> CACHE = new HashMap<>();
+
+	static final BiFunction<Number, Integer, Number> DEFAULT_ADDER = (t, addingValue) -> {
+		if (t instanceof Integer i) {
+			return Integer.valueOf((i + addingValue));
+		}
+		if (t instanceof Long l) {
+			return Long.valueOf((l + addingValue));
+		}
+		throw new IllegalStateException("unsupported type");
+	};
+
 	final T x;
 
 	final T y;
 
+	@Nullable
 	private final BiFunction<T, Integer, T> adder;
+
+	/**
+	 * Ensure instances are unique, improve memory usage
+	 */
+	private static <T extends Number> Point2D<T> lookupCache(final T x,
+															 final T y,
+															 final Supplier<Point2D<T>> creator) {
+		return (Point2D<T>) CACHE.computeIfAbsent(x, _ -> new HashMap<>())
+								 .computeIfAbsent(y, _ -> creator.get());
+	}
 
 	public static <T extends Number> Point2D<T> create(
 			final T x,
 			final T y
 	) {
+		return lookupCache(x, y, () -> create0(x, y, null));
+	}
+
+	static <T extends Number> Point2D<T> create(
+			final T x,
+			final T y,
+			@Nullable final BiFunction<T, Integer, T> adder
+	) {
+		return lookupCache(x, y, () -> create0(x, y, adder));
+	}
+
+	static <T extends Number> Point2D<T> create0(
+			final T x,
+			final T y,
+			@Nullable final BiFunction<T, Integer, T> adder
+	) {
 		return new Point2D<>(
 				x,
 				y,
-				(t, v) -> {
-					if (t instanceof Integer i) {
-						return (T) Integer.valueOf((i + v));
-					}
-					if (t instanceof Long l) {
-						return (T) Long.valueOf((l + v));
-					}
-					throw new IllegalStateException("unsupported type");
-				}
+				adder
 		);
-	}
-
-	public static <T extends Number> Point2D<T> create(
-			final T x,
-			final T y,
-			final BiFunction<T, Integer, T> adder
-	) {
-		return new Point2D<>(x, y, adder);
 	}
 
 	public static Point2D<Integer> createInt(
 			final int x,
 			final int y
 	) {
-		return new Point2D<>(x, y, Integer::sum);
+		return create(x, y, Integer::sum);
 	}
 
-	public Point2D(final T x,
-				   final T y,
-				   final BiFunction<T, Integer, T> adder) {
+	Point2D(final T x,
+			final T y,
+			@Nullable final BiFunction<T, Integer, T> adder) {
 		this.x = x;
 		this.y = y;
 		this.adder = adder;
+	}
+
+	BiFunction<T, Integer, T> adder() {
+		return requireNonNullElse(adder, (BiFunction<T, Integer, T>) DEFAULT_ADDER);
 	}
 
 	public Stream<Point2D<T>> untilX(final Point2D<T> other) {
@@ -65,7 +98,7 @@ public class Point2D<T extends Number> {
 		final var otherLong = other.getX().longValue();
 		while (temp.getX().longValue() <= otherLong) {
 			result.add(temp);
-			temp = createNew(adder.apply(temp.x, 1), temp.y);
+			temp = createNew(adder().apply(temp.x, 1), temp.y);
 		}
 		return result.stream();
 	}
@@ -76,7 +109,7 @@ public class Point2D<T extends Number> {
 		final var otherLong = other.getY().longValue();
 		while (temp.getY().longValue() <= otherLong) {
 			result.add(temp);
-			temp = createNew(temp.x, adder.apply(temp.y, 1));
+			temp = createNew(temp.x, adder().apply(temp.y, 1));
 		}
 		return result.stream();
 	}
@@ -141,27 +174,27 @@ public class Point2D<T extends Number> {
 	public Point2D<T> up() {
 		return createNew(
 				x,
-				adder.apply(y, -1)
+				adder().apply(y, -1)
 		);
 	}
 
 	public Point2D<T> upLeft() {
 		return createNew(
-				adder.apply(x, -1),
-				adder.apply(y, -1)
+				adder().apply(x, -1),
+				adder().apply(y, -1)
 		);
 	}
 
 	public Point2D<T> upRight() {
 		return createNew(
-				adder.apply(x, 1),
-				adder.apply(y, -1)
+				adder().apply(x, 1),
+				adder().apply(y, -1)
 		);
 	}
 
 	public Point2D<T> right() {
 		return createNew(
-				adder.apply(x, 1),
+				adder().apply(x, 1),
 				y
 		);
 	}
@@ -169,29 +202,34 @@ public class Point2D<T extends Number> {
 	public Point2D<T> down() {
 		return createNew(
 				x,
-				adder.apply(y, +1)
+				adder().apply(y, +1)
 		);
 	}
 
 	public Point2D<T> downLeft() {
 		return createNew(
-				adder.apply(x, -1),
-				adder.apply(y, 1)
+				adder().apply(x, -1),
+				adder().apply(y, 1)
 		);
 	}
 
 	public Point2D<T> downRight() {
 		return createNew(
-				adder.apply(x, 1),
-				adder.apply(y, 1)
+				adder().apply(x, 1),
+				adder().apply(y, 1)
 		);
 	}
 
 	public Point2D<T> left() {
 		return createNew(
-				adder.apply(x, -1),
+				adder().apply(x, -1),
 				y
 		);
+	}
+
+	@Override
+	public String toString() {
+		return "(%s/%s)".formatted(x, y);
 	}
 
 }
