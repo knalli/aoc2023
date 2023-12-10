1fba55cc3d20c8e5228aed0a89e9405052d365ae
knalli <knallisworld@googlemail.com>
2023-12-10 15:11:04 +0100

    Also enrich FixGrid with better getValue and adjacents tooling

--- src/main/java/de/knallisworld/aoc2023/support/geo/grid2/FixGrid.java
+++ src/main/java/de/knallisworld/aoc2023/support/geo/grid2/FixGrid.java
@@ -3,11 +3,18 @@ package de.knallisworld.aoc2023.support.geo.grid2;
 import de.knallisworld.aoc2023.support.geo.Point2D;
 
 import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
+import static java.util.function.Predicate.not;
 
 public class FixGrid<T> {
 
@@ -40,6 +47,12 @@ public class FixGrid<T> {
 	}
 
 	public boolean hasValue(final int x, final int y) {
+		if (!(0 <= y && y < data.length)) {
+			return false;
+		}
+		if (!(0 <= x && x < data[y].length)) {
+			return false;
+		}
 		return data[y][x] != null;
 	}
 
@@ -49,6 +62,18 @@ public class FixGrid<T> {
 
 	public T getValueRequired(final int x, final int y) {
 		return requireNonNull(data[y][x]);
+	}
+
+	public Optional<T> getValue(final Point2D<Integer> p) {
+		return getValue(p.getX(), p.getY());
+	}
+
+	public Optional<T> getValue(final int x, final int y) {
+		if (hasValue(x, y)) {
+			return Optional.of(requireNonNull(data[y][x]));
+		} else {
+			return Optional.empty();
+		}
 	}
 
 	public FieldsView<T> fields() {
@@ -83,6 +108,52 @@ public class FixGrid<T> {
 			stream().forEach(consumer);
 		}
 
+		public Stream<Point2D<Integer>> getAdjacents4(final Point2D<Integer> p) {
+			return p.getAdjacents4()
+					.filter(grid::hasValue);
+		}
+
+		public Stream<Point2D<Integer>> getCluster4(final Point2D<Integer> p,
+													final Predicate<Field<T>> filter) {
+
+			final var cluster = new HashSet<Point2D<Integer>>();
+			final var q = new LinkedList<Point2D<Integer>>();
+			q.add(p);
+			while (!q.isEmpty()) {
+				final var n = q.pop();
+				cluster.add(n);
+				getAdjacents4(n)
+						.filter(not(cluster::contains))
+						.filter(not(q::contains))
+						.filter(a -> filter.test(new Field<>(a, grid.getValueRequired(a))))
+						.forEach(q::add);
+			}
+			return cluster.stream();
+		}
+
+		public Stream<Point2D<Integer>> getAdjacents8(final Point2D<Integer> p) {
+			return p.getAdjacents8()
+					.filter(grid::hasValue);
+		}
+
+	}
+
+	public String toString(final BiFunction<Point2D<Integer>, T, String> renderer) {
+
+		final var sb = new StringBuilder();
+
+		IntStream.range(0, data.length)
+				 .forEach(y -> {
+					 IntStream.range(0, data[y].length)
+							  .forEach(x -> {
+								  final var p = Point2D.create(x, y);
+								  final var v = getValueRequired(p);
+								  sb.append(renderer.apply(p, v));
+							  });
+					 sb.append("\n");
+				 });
+
+		return sb.toString();
 	}
 
 }
