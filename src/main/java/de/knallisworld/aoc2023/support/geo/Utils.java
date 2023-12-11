2945d4360e82a4fbbb90668a174dbca39d3127eb
knalli <knallisworld@googlemail.com>
2023-12-11 13:05:54 +0100

    Fix bug in manhattenDistance :(((((

--- src/main/java/de/knallisworld/aoc2023/support/geo/Utils.java
+++ src/main/java/de/knallisworld/aoc2023/support/geo/Utils.java
@@ -8,7 +8,7 @@ public class Utils {
 	@SuppressWarnings("SpellCheckingInspection")
 	public static <T extends Number> long manhattenDistance(final Point2D<T> a,
 															final Point2D<T> b) {
-		return abs(b.getX().longValue() - a.getX().longValue() + abs(b.getY().longValue()) - a.getY().longValue());
+		return abs(b.getX().longValue() - a.getX().longValue()) + abs(b.getY().longValue() - a.getY().longValue());
 	}
 
 }
